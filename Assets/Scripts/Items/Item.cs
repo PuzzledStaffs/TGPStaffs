5cca386a33295460d1f2fb890cebbc12625331c8
Gabi4213 <m015103k@student.staffs.ac.uk>
2022-01-10 16:28:28 +0000

    Basic Item Implementation
    Created an item scriptable object. this item can be created by right clicking then select weapons and then finally item. The item has some basic info you can fill in right now but more will be added.
    Inside of the scene "GabiScene" you can toggle a wepaon wheel by pressting Tab.  Inside the weapon wheel there are buttons with a function to select item. On there you can change what item is assigned to the weapon wheel part thing. Currently if you select an item on the weapon wheel it will just display what item you selected in the console.

---
+++ Assets/Scripts/Items/Item.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Item", menuName = "Weapons/item")]
+public class Item : ScriptableObject
+{
+    new public string name = "Item Name";
+    public Sprite ItemIcon = null;
+    public float ItemDamage = 0.0f;
+    public int CombatType = 0;
+
+
+    public virtual void ItemActionRightClick(int Type)
+    {
+        switch (Type)
+        {
+            case 0: //Sword
+                Debug.Log("Sword Right Click Attack");
+                break;
+            case 1: //Bow
+                Debug.Log("Bow Right Click Attack");
+                break;
+        }
+    }
+
+    public virtual void ItemActionLeftClick(int Type)
+    {
+        switch (Type)
+        {
+            case 0: //Sword
+                Debug.Log("Sword Left Click Attack");
+                break;
+            case 1: //Bow
+                Debug.Log("Bow Left Click Attack");
+                break;
+        }
+    }
+
+
+
+}
---
+++ Assets/Scripts/Items/WeaponWheelController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class WeaponWheelController : MonoBehaviour
+{
+    public int ItemSelected;
+    public GameObject WeaponWheel;
+    Item CurrentSelectItem = null;
+
+
+    public void ToggleWheel() 
+    {
+        if (WeaponWheel.activeSelf)
+        {
+            WeaponWheel.SetActive(false);
+        }
+        else
+        {
+            WeaponWheel.SetActive(true);
+        }
+
+    }
+
+    private void Update()
+    {
+        // this is temporary for testing - cause i didnt want to set up the input stuff properly yet
+        var keyboard = Keyboard.current;
+
+        if (keyboard.tabKey.wasPressedThisFrame)
+        {
+            ToggleWheel();
+        }
+    }
+
+
+    public void SelectItem(Item itemSelected)
+    {
+        CurrentSelectItem = itemSelected;
+        Debug.Log("Item Selected: " + itemSelected.name);
+    }
+}
