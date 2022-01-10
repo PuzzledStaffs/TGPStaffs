1ea0759ec9ceb03c6b394ba49337a0c301d34cc6
Izzy Groom <69197054+IsabellaGroom@users.noreply.github.com>
2022-01-10 14:59:06 +0000

    Basic Enemy class
    Basic enemy class, logic for health and damage mechanics

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour, IHealth
+{
+    int m_health = 50;
+
+    public int GetHealth()
+    {
+        return m_health;
+    }
+
+    public bool isDead(int health)
+    {
+        if (health <= 0)
+        {
+            return true;
+        }
+        else 
+        {
+            return false;
+
+        }
+
+
+    }
+
+    public void TakeDamage(int damage)
+    {
+        m_health -= damage;
+
+        if (isDead(m_health))
+        {
+            StartCoroutine(DeathCoroutine());
+        }
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      //get fsm
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //update state
+    }
+
+    IEnumerator DeathCoroutine()
+    {
+        //TODO: Add death animation
+        yield return new WaitForSeconds(5);
+        Destroy(gameObject);
+    }
+
+
+
+}
