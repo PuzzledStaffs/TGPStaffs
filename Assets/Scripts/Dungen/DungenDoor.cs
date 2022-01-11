5e492c6f09b4eb91f9b1b72e541cff88064662c2
WilliamIMiller <71334596+WilliamIMiller@users.noreply.github.com>
2022-01-11 16:48:18 +0000

    Doors open and clise

--- Assets/Scripts/Dungen/DungenDoor.cs
+++ Assets/Scripts/Dungen/DungenDoor.cs
@@ -7,10 +7,24 @@ public class DungenDoor : MonoBehaviour
     [SerializeField] private GameObject m_toRoomExitPoint;
     [SerializeField] private GameObject m_toRoomCameraMove;
     private DungenManager m_dungenManager;
+    [Header("Door Closed Controls")]
+    private Renderer m_doorRenderer;
+    private BoxCollider m_DoorColider;
+    [SerializeField] Material m_DoorClosed;
+    [SerializeField] Material m_DoorOpen;
+    public bool m_doorActive { get; protected set; }    
+    
 
     private void Awake()
     {
         m_dungenManager = GameObject.FindObjectOfType<DungenManager>();
+        m_doorRenderer = transform.GetComponent<Renderer>();
+        m_DoorColider = transform.GetComponent<BoxCollider>();
+        OpenDoor();
+        if (m_toRoomCameraMove == null && m_toRoomExitPoint == null)
+        {
+            CloseDoor();
+        }
     }
 
     private void OnTriggerEnter(Collider other)
@@ -25,5 +39,22 @@ public class DungenDoor : MonoBehaviour
             }
         }
     }
+
+    #region Door Controls
+
+    public void OpenDoor()
+    {
+        m_doorActive = true;
+        m_doorRenderer.material = m_DoorOpen;
+        m_DoorColider.isTrigger = true;
+    }
+
+    public void CloseDoor()
+    {
+        m_doorActive = false;
+        m_doorRenderer.material = m_DoorClosed;
+        m_DoorColider.isTrigger = false;
+    }
+
+    #endregion
 }
- 
