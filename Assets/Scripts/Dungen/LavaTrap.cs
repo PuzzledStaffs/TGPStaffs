cf407c8622a37a9625d7b472295d46c4780e305f
William Miller <71334596+WilliamIMiller@users.noreply.github.com>
2022-01-13 15:50:35 +0000

    lava trap

--- Assets/Scripts/Dungen/LavaTrap.cs
+++ Assets/Scripts/Dungen/LavaTrap.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(BoxCollider))]
 public class LavaTrap : MonoBehaviour
 {
     [SerializeField] private bool m_AmBrigde;
