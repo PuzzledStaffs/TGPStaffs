4e92504ff981dec2237e6bcbcec8912ab0f0335b
Izzy Groom <69197054+IsabellaGroom@users.noreply.github.com>
2022-01-13 15:55:58 +0000

    Added Death and restart mechanics
    -Player can die
    -Currently restarts scene

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,191 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour, IHealth
+{
+    [Header("Components"), SerializeField]
+    private Rigidbody m_rigidbody;
+    public GameObject m_model;
+
+    [Header("Health and Death")]
+    public Vector3 m_respawnPosition;
+    int m_health = 100;
+
+    [Header("Movement")]
+    [SerializeField, ReadOnly]
+    Vector2 m_moveDir = new Vector2();
+    [SerializeField, ReadOnly]
+    private bool m_movementFrozen = false;
+    public float m_speed = 5.0f;
+
+    [Header("Weapon Wheel")]
+    [SerializeField, ReadOnly]
+    Vector2 m_pointerPos;
+    [SerializeField]
+    WeaponWheelController m_weaponWheelController;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        m_respawnPosition = transform.position;
+    }
+
+    void Update()
+    {
+        if (!m_movementFrozen)
+        {
+            // Gravity is handled by Rigidbody
+            m_rigidbody.velocity = new Vector3(m_moveDir.x * m_speed, m_rigidbody.velocity.y, m_moveDir.y * m_speed);
+
+            // Rotates model to face the direction of movement
+            if (m_moveDir.x != 0 || m_moveDir.y != 0)
+                m_model.transform.rotation = Quaternion.LookRotation(new Vector3(m_moveDir.x, 0.0f, m_moveDir.y), Vector3.up);
+        }
+    }
+
+    #region Freezing Movement
+    public void FreezeMovement()
+    {
+        m_movementFrozen = true;
+        m_rigidbody.velocity = new Vector3(0, m_rigidbody.velocity.y, 0);
+    }
+
+    public void UnFreezeMovement()
+    {
+        m_movementFrozen = false;
+    }
+    #endregion
+
+    #region Input Action Events
+    /// <summary>
+    /// Unity Input Action callback for movement
+    /// </summary>
+    public void OnMove(InputAction.CallbackContext ctx)
+    {
+        m_moveDir = ctx.ReadValue<Vector2>();
+    }
+
+    /// <summary>
+    /// Unity Input Action callback for movement
+    /// </summary>
+    public void OnPointerMove(InputAction.CallbackContext ctx)
+    {
+        m_pointerPos = ctx.ReadValue<Vector2>();
+    }
+
+    /// <summary>
+    /// Unity Input Action callback for use button
+    /// </summary>
+    public void OnUse(InputAction.CallbackContext ctx)
+    {
+        // Check the phase of the button press. Equivalent to if ctx.started else if ctx.performed else if ctx.canceled
+        switch (ctx.phase)
+        {
+            // Button was pressed
+            case InputActionPhase.Started:
+                // Gets all objects with a collider in a box (halfExtents = scale / 2) in front of the player
+                foreach (Collider col in Physics.OverlapBox(transform.position + m_model.transform.forward, new Vector3(1.0f, 1.0f, 1.0f) / 2, m_model.transform.rotation))
+                    // If the collider also has a IInteractable script, interact with it
+                    col.GetComponent<IInteractable>()?.Interact();
+
+                break;
+            // Button is being held
+            case InputActionPhase.Performed:
+            // Button was released
+            case InputActionPhase.Canceled:
+            case InputActionPhase.Disabled:
+            case InputActionPhase.Waiting:
+            default:
+                break;
+        }
+
+    }
+
+    /// <summary>
+    /// Unity Input Action callback for use button
+    /// </summary>
+    public void OnToggleWeaponWheel(InputAction.CallbackContext ctx)
+    {
+        // Check the phase of the button press. Equivalent to if ctx.started else if ctx.performed else if ctx.canceled
+        switch (ctx.phase)
+        {
+            // Button was pressed
+            case InputActionPhase.Started:
+                m_weaponWheelController.ToggleWheel();
+                break;
+            // Button is being held
+            case InputActionPhase.Performed:
+            // Button was released
+            case InputActionPhase.Canceled:
+            case InputActionPhase.Disabled:
+            case InputActionPhase.Waiting:
+            default:
+                break;
+        }
+
+    }
+    #endregion
+
+    #region Player Health And Death
+    /// <summary>
+    /// Teleports the player to their repsawn position.
+    /// Not to be confused with death functions.
+    /// </summary>
+    public void Respawn()
+    {
+        transform.position = m_respawnPosition;
+    }
+
+    public int GetHealth()
+    {
+        int health = m_health;
+    
+        return health;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        m_health -= damage;
+
+        if (isDead(m_health))
+        {
+            StartCoroutine(DeathCoroutine());
+        }
+
+    }
+    public bool isDead(int health)
+    {
+        if (health <= 0)
+        {
+            return true;
+        }
+        else 
+        {
+            return false;
+
+        }
+    }
+
+
+    IEnumerator DeathCoroutine()
+    {
+        yield return new WaitForSeconds(5);
+        Restart();
+    }
+
+
+    public void Restart()
+    {
+        //TODO: Change this to appropriate scene
+        SceneManager.LoadScene("IzzyScene");
+    }
+
+
+
+
+    #endregion
+}
