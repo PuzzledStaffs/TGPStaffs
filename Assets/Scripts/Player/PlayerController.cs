f3ad69536592da3e36622ead2b9891abdad2f740
KitSymes <74962587+KitSymes@users.noreply.github.com>
2022-01-17 10:57:44 +0000

    Start Alt Interact + Dungeon Camera Fixes

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -25,12 +25,14 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     WeaponWheelController m_weaponWheelController;
 
-    public bool ButtonHeld = false;
+    public bool m_buttonHeld = false;
 
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
-        //Cursor.visible = false;
+#if !UNITY_EDITOR
+        Cursor.visible = false;
+#endif
         m_respawnPosition = transform.position;
     }
 
@@ -98,19 +100,58 @@ public class PlayerController : MonoBehaviour
         {
             // Button was pressed
             case InputActionPhase.Started:
+                // Prevents the player from using items whilst interacting
+                if (m_buttonHeld)
+                    break;
+
                 if (m_weaponWheelController.isWheelOpen)
                 {
                     m_weaponWheelController.SelectItem(m_weaponWheelController.currentIndex);
                     break;
                 }
-                
+
                 if (!m_weaponWheelController.CurrentItem.ItemHold)
-                {
                     m_weaponWheelController.LeftClickAction();
-                }
                 else
+                    m_buttonHeld = true;
+                break;
+            // Button is being held
+            case InputActionPhase.Performed:
+                break;
+            // Button was released
+            case InputActionPhase.Canceled:
+                if (m_buttonHeld)
                 {
-                    ButtonHeld = true;
+                    m_buttonHeld = false;
+                    m_weaponWheelController.HoldActionCooldown();
+                }
+                break;
+            case InputActionPhase.Disabled:
+            case InputActionPhase.Waiting:
+            default:
+                break;
+        }
+
+    }
+
+    /// <summary>
+    /// Unity Input Action callback for alternate use button
+    /// </summary>
+    public void OnAltInteract(InputAction.CallbackContext ctx)
+    {
+        // Check the phase of the button press. Equivalent to if ctx.started else if ctx.performed else if ctx.canceled
+        switch (ctx.phase)
+        {
+            // Button was pressed
+            case InputActionPhase.Started:
+                // Prevents the player from interacting when using or changing items
+                if (m_buttonHeld || m_weaponWheelController.isWheelOpen)
+                    break;
+
+                foreach (Collider col in Physics.OverlapBox(transform.position + m_model.transform.forward, new Vector3(1.0f, 1.0f, 1.0f) / 2, m_model.transform.rotation))
+                {
+                    if (col.CompareTag("Player"))
+                        continue;
                 }
                 break;
             // Button is being held
@@ -118,11 +159,8 @@ public class PlayerController : MonoBehaviour
                 break;
             // Button was released
             case InputActionPhase.Canceled:
-                if (ButtonHeld)
-                {
-                    ButtonHeld = false;
-                    m_weaponWheelController.HoldActionCooldown();
-                }
+                if (m_buttonHeld)
+                    m_buttonHeld = false;
                 break;
             case InputActionPhase.Disabled:
             case InputActionPhase.Waiting:
@@ -142,6 +180,10 @@ public class PlayerController : MonoBehaviour
         {
             // Button was pressed
             case InputActionPhase.Started:
+                // Prevents the player from opening the weapon wheel whilst interacting or using an item
+                if (m_buttonHeld)
+                    break;
+
                 m_weaponWheelController.ToggleWheel();
                 break;
             // Button is being held
