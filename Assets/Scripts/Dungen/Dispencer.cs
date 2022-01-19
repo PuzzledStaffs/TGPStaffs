89796ed5ed5954cfbc302ee8bd1f44d32cb1989b
WilliamIMiller <71334596+WilliamIMiller@users.noreply.github.com>
2022-01-19 11:43:28 +0000

    Dispenser Update

--- Assets/Scripts/Dungen/Dispencer.cs
+++ Assets/Scripts/Dungen/Dispencer.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Dispencer : Trap
@@ -7,7 +8,14 @@ public class Dispencer : Trap
     [SerializeField] private GameObject m_spawnedObject;
     [SerializeField][Min(1)] private int m_MaxiumInstances;
     [SerializeField] [ReadOnly] List<GameObject> m_spawnedThings = new List<GameObject>();
+    [SerializeField] TextMeshProUGUI m_TopText;
+    [SerializeField] [Tooltip("The despencer will drop only the maximun instances, any more and it will not spawn now overriding the oldest")]
+    private bool m_NoReset;
 
+    private void Start()
+    {
+        m_TopText.text = m_MaxiumInstances.ToString();
+    }
 
     public override void EnterRoomEnabled()
     {
@@ -20,15 +28,36 @@ public class Dispencer : Trap
 
     public void SpawnItem()
     {
+        //Spawn Item
         if(m_spawnedThings.Count < m_MaxiumInstances)
         {
             m_spawnedThings.Add(Instantiate(m_spawnedObject, transform.forward/2 + transform.position, transform.rotation));
+            
+        }
+        else if(m_NoReset)
+        {
+            
         }
         else
         {
             Destroy(m_spawnedThings[0]);
             m_spawnedThings.RemoveAt(0);
             m_spawnedThings.Add(Instantiate(m_spawnedObject, transform.forward / 2 + transform.position, transform.rotation));
+        }
+
+        //Update Top Text
+        m_TopText.text = (m_MaxiumInstances - m_spawnedThings.Count).ToString();
+        if (m_TopText.text == "0")
+        {
+            m_TopText.color = Color.red;
+            if(m_NoReset)
+            {
+                m_TopText.text = "X";
+            }
+        }
+        else
+        {
+            m_TopText.color = Color.white;
         }
     }
 }
