5fc724468d745b00ff22b5180f0faa18e7d5afb7
WilliamIMiller <71334596+WilliamIMiller@users.noreply.github.com>
2022-01-19 14:25:01 +0000

    pressure plat ajustment

--- Assets/Scripts/PressurePlate.cs
+++ Assets/Scripts/PressurePlate.cs
@@ -21,7 +21,7 @@ public class PressurePlate : MonoBehaviour
             m_onPlate.Add(other);
             other.GetComponentInParent<KickCube>().m_OnRemoveSelf += RemoveList;
         }
-        if (m_onPlate.Count > 0)
+        if (m_onPlate.Count == 1)
             m_platePressed.Invoke();
     }
 
