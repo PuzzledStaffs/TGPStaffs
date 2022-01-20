4d0a973a263ed11d744be099153081dab9388e89
Alexandru Ionescu <71647115+IonescuAlexandruM@users.noreply.github.com>
2022-01-20 11:11:01 +0000

    Arrow now destroys itself on collision

--- Assets/Scripts/Items/Arrow.cs
+++ Assets/Scripts/Items/Arrow.cs
@@ -14,7 +14,7 @@ public class Arrow : MonoBehaviour
         if(transform.position != EndPoint)
         {
             float speed = (bowParent.ArrowSpeed + bowParent.CurrentRange) * Time.deltaTime;
-            transform.position = Vector3.MoveTowards(transform.position, EndPoint, speed);
+            transform.position = Vector3.MoveTowards(transform.position, new Vector3(EndPoint.x,transform.position.y,EndPoint.z), speed);
         }
         else if(transform.position == EndPoint)
         {
@@ -40,7 +40,10 @@ public class Arrow : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
+        if (collision.transform.tag == "Player") { return; }
+
         collision.gameObject.GetComponent<IInteractable>()?.Interact();
         collision.gameObject.GetComponent<IHealth>()?.TakeDamage(bowParent.ItemDamage);
+        KillArrow();
     }
 }
