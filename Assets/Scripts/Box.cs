aeb7b68f3c8cb5030b37cdad289a93fb97d41dbe
KitSymes <kitsymes@btinternet.com>
2022-01-24 17:53:59 +0000

    Box Collision
    - Can no longer move a box into another box
    - Fixed other box related issues like direction and only in facing axis caused by me simplifying code

--- Assets/Scripts/Box.cs
+++ Assets/Scripts/Box.cs
@@ -9,7 +9,7 @@ public class Box : MonoBehaviour
 
     Transform m_tiles;
     float xOffset;
-    float zOffset;
+    float yOffset;
 
     public float m_boxLerpTime;
     public Vector3 m_boxLerpStart;
@@ -21,15 +21,15 @@ public class Box : MonoBehaviour
     {
         m_tiles = m_myRoom.transform.Find("Floor").Find("Box Tiles");
         xOffset = m_myRoom.m_RoomType == RoomType.NORMAL ? -4.5f : -9.0f;
-        zOffset = m_myRoom.m_RoomType == RoomType.NORMAL ? -9.5f : -19.0f;
+        yOffset = m_myRoom.m_RoomType == RoomType.NORMAL ? -9.5f : -19.0f;
         m_boxLerpStart = transform.position;
         m_boxLerpEnd = transform.position;
         m_boxLerpTime = 0.0f;
         m_moving = false;
-        if (!GetTile(new Vector3()))
+        if (!IsValidTile(new Vector3()))
         {
-            Debug.LogError("Box is not on a tile");
-            gameObject.SetActive(false);
+            Debug.LogError("Box " + name + " is not on a tile");
+            //gameObject.SetActive(false);
             return;
         }
     }
@@ -43,7 +43,8 @@ public class Box : MonoBehaviour
                 m_boxLerpEnd.y = transform.position.y;
                 transform.position = Vector3.Lerp(m_boxLerpStart, m_boxLerpEnd, m_boxLerpTime);
                 m_boxLerpTime += Time.deltaTime * 2.0f;
-            } else
+            }
+            else
             {
                 transform.position = m_boxLerpEnd;
                 m_moving = false;
@@ -51,15 +52,14 @@ public class Box : MonoBehaviour
         }
     }
 
-    public bool GetTile(Vector3 offset)
+    public bool IsValidTile(Vector3 offset)
     {
-        Vector3 localOffset = transform.InverseTransformDirection(offset);
-        // Keep in mind pX is grid y and pZ is grid x
-        int pX = Mathf.RoundToInt(transform.localPosition.x - localOffset.x - xOffset), pZ = Mathf.RoundToInt(transform.localPosition.z + localOffset.z - zOffset);
+        // Yes, the grid y is x and grid x is z. No, I don't remember why. I think it was because the room was created rotated, and so the top left tile actually is the top right in code
+        int gridY = GetTileY(offset), gridX = GetTileX(offset);
         foreach (Transform tr in m_tiles)
         {
-            int tX = Mathf.RoundToInt(tr.localPosition.x - xOffset), tZ = Mathf.RoundToInt(tr.localPosition.z - zOffset);
-            if (pX == tX && pZ == tZ)
+            int trGridY = Mathf.RoundToInt(tr.localPosition.x - xOffset), trGridX = Mathf.RoundToInt(tr.localPosition.z - yOffset);
+            if (gridY == trGridY && gridX == trGridX)
                 return true;
         }
         return false;
@@ -72,5 +72,32 @@ public class Box : MonoBehaviour
         m_boxLerpEnd = new Vector3(transform.position.x + mov.z, transform.position.y, transform.position.z + mov.x);
         m_boxLerpTime = 0.0f;
         m_moving = true;
+    }
+
+    public int GetTileX()
+    {
+        return GetTileX(new Vector3());
+    }
+
+    public int GetTileY()
+    {
+        return GetTileY(new Vector3());
+    }
+
+    public int GetTileX(Vector3 offset)
+    {
+        Vector3 localOffset = transform.InverseTransformDirection(offset);
+        return Mathf.RoundToInt(transform.localPosition.z - yOffset - localOffset.x);
+    }
+
+    public int GetTileY(Vector3 offset)
+    {
+        Vector3 localOffset = transform.InverseTransformDirection(offset);
+        return Mathf.RoundToInt(transform.localPosition.x - xOffset - localOffset.z);
+    }
+
+    public bool Occupies(int tileX, int tileY)
+    {
+        return tileX == GetTileX() && tileY == GetTileY();
     }
 }
