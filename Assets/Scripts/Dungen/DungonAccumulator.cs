694ea0b51a8c57770505be32de84928f7f32504b
WilliamIMiller <71334596+WilliamIMiller@users.noreply.github.com>
2022-01-26 16:06:16 +0000

    1.3 for kit

--- Assets/Scripts/Dungen/DungonAccumulator.cs
+++ Assets/Scripts/Dungen/DungonAccumulator.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEditor.Timeline.Actions;
 using UnityEngine;
 using UnityEngine.Events;
+using TMPro;
+using Unity.VisualScripting;
 
 public class DungonAccumulator : MonoBehaviour
 {
@@ -11,26 +13,39 @@ public class DungonAccumulator : MonoBehaviour
 
 
     [SerializeField] private int m_ExpectedInputs;
+    [SerializeField] private TextMeshProUGUI m_Text;
     private int m_currentInputs = 0;
     private bool m_active = false;
+
+    private void Start()
+    {
+        m_Text.text = m_currentInputs.ToString();
+        m_Text.color = Color.red;
+    }
+
 
     public void AddTo()
     {
         m_currentInputs++;
+        m_Text.text = m_currentInputs.ToString();
+        
         if (m_currentInputs >= m_ExpectedInputs && !m_active)
         {
             m_Acumilated?.Invoke();
             m_active = true;
+            m_Text.color = Color.green;
         }
     }
 
     public void RemoveTo()
     {
         m_currentInputs--;
+        m_Text.text = m_currentInputs.ToString();
         if (m_currentInputs < m_ExpectedInputs && m_active)
         {
             m_active = false;
             m_UnAcumilated?.Invoke();
+            m_Text.color = Color.red;
         }
     }
 }
