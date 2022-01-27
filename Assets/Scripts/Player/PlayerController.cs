69c33b33ebb594d2de50173ba5b60742f85e15c9
William Miller <71334596+WilliamIMiller@users.noreply.github.com>
2022-01-27 12:57:41 +0000

    minor touches

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -62,8 +62,6 @@ public class PlayerController : MonoBehaviour, IHealth
     private float m_deathLerpTime = 0.0f;
     public LineRenderer BowLineRenderer;
 
-    [Header("For The Grass")]
-    public Material Grass;
     [SerializeField] float RadiusOfTrample;
 
     void Awake()
@@ -78,8 +76,7 @@ public class PlayerController : MonoBehaviour, IHealth
     }
 
     void Update()
-    {
-        Grass?.SetVector("_GrassTrample", new Vector4(transform.position.x, transform.position.y + 2f, transform.position.z, RadiusOfTrample));
+    {     
         if (m_weaponWheelController.isWheelOpen)
         {
             Vector2 direction = m_pointerPos;
