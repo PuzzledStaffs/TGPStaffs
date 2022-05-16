65c02287594e8bd34240d2620ba6ed083851f65a
Izzy Groom <69197054+IsabellaGroom@users.noreply.github.com>
2022-05-16 12:07:12 +0100

    enemy update

--- Assets/Scripts/Enemy/EnemyController.cs
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class EnemyController : MonoBehaviour, IHealth
+public class EnemyController : State, IHealth
 {
     [Header("---------------Enemy Statistics------------------------------------------------------------")]
     public int m_health = 50;
@@ -21,7 +21,8 @@ public class EnemyController : MonoBehaviour, IHealth
     public Vector3 m_offset;
     public float m_dodgeChance = 0;
     private bool m_takingDamage = false;
-    public int m_currentState = 0;
+   // public int m_currentState = 0;
+    public StateType m_stateType;
 
     public LayerMask m_whatIsPlayer, m_whatIsGround;
 
@@ -32,13 +33,15 @@ public class EnemyController : MonoBehaviour, IHealth
 
     private void Start()
     {
+        m_player = GameObject.FindGameObjectWithTag("Player").transform;
+        m_stateType = StateType.CHASE;
         m_pathToPlayer = new NavMeshPath();
+        Debug.Log("calculate 1");
         InvokeRepeating("CalculatePath", 0.5f, 0.5f);
     }
 
     private void Awake()
-    {
-        m_player = GameObject.Find("Player").transform;
+    {       
         m_agent = GetComponent<NavMeshAgent>();
         // animator = GetComponent<Animator>();
         m_agent.updatePosition = false;
@@ -60,8 +63,10 @@ public class EnemyController : MonoBehaviour, IHealth
     }
     void CalculatePath()
     {
-        if (m_currentState == 2)
+        Debug.Log("calculate 2");
+        if(m_stateType == StateType.CHASE)//if (m_currentState == 2)
         {
+            Debug.Log("calculate 3");
             m_agent.Warp(transform.position);
             m_pathToPlayer = new NavMeshPath();
             m_agent.CalculatePath(m_player.position, m_pathToPlayer);
@@ -70,18 +75,20 @@ public class EnemyController : MonoBehaviour, IHealth
 
     void ChasePlayer()
     {
-        if (m_pathToPlayer.corners.Length > 1 && !m_died && m_currentState != 4)
+        //if (m_pathToPlayer.corners.Length > 1 && !m_died && m_currentState != 4)
+        if (m_pathToPlayer.corners.Length > 1 && !m_died && m_stateType != StateType.IDLE)
         {
             Debug.Log("Chase State");
-            m_currentState = 2;
-            if (!m_takingDamage || m_currentState != 3) // walking
-            {
+           // m_currentState = 2;
+            m_stateType = StateType.CHASE;
+          //  if (!m_takingDamage || m_currentState != 3) // walking
+         //   {
                 
-            }
+        //    }
             //Apply Force in the direction fo the next point
             Vector3 dir = (m_pathToPlayer.corners[1] - transform.position).normalized;
             var rotation = Quaternion.LookRotation(new Vector3(dir.x, dir.y, dir.z));
-            transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * 2);
+            transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * 2);           
             m_rb.velocity = (Vector3.ClampMagnitude(m_rb.velocity, 3f));
             m_rb.AddForce(dir * m_speed);
             if (Vector3.Distance(transform.position, new Vector3(m_pathToPlayer.corners[1].x, transform.position.y, m_pathToPlayer.corners[1].z)) < 6)
@@ -96,7 +103,8 @@ public class EnemyController : MonoBehaviour, IHealth
         if (!m_died)
         {
             m_rb.velocity = new Vector3(0, 0, 0);
-            m_currentState = 3;
+           // m_currentState = 3;
+            m_stateType = StateType.ATTACK;
             transform.LookAt(m_player);
             //not walking
         }
@@ -121,17 +129,20 @@ public class EnemyController : MonoBehaviour, IHealth
 
     IEnumerator StopMoving()
     {
-        m_currentState = 4;
+      //  m_currentState = 4;
+        m_stateType = StateType.IDLE;
         m_rb.velocity = new Vector3(0, 0, 0);
         //yield return new WaitForSeconds(1.8f);
         yield return new WaitForSeconds(1f);
-        m_currentState = 2;
+     //   m_currentState = 2;
+        m_stateType = StateType.CHASE;
         m_takingDamage = false;
     }
 
     public void Dodge()
     {
-        if (m_currentState != 4) //if not equal to take damage state
+        if(m_stateType != StateType.IDLE)
+        //if (m_currentState != 4) //if not equal to take damage state
         {
             m_dodgeChance += 1 * Time.deltaTime;
             float randomNum = Random.Range(0, 1000 - (m_dodgeChance * 200));
@@ -168,7 +179,8 @@ public class EnemyController : MonoBehaviour, IHealth
         yield return new WaitForSeconds(time);
         m_health -= damage.damageAmount;
         m_animator.SetTrigger("EnemyHit");
-        m_currentState = 4; //sets the state to take damage
+      //  m_currentState = 4; //sets the state to take damage
+        m_stateType = StateType.IDLE;
         m_takingDamage = true;
         //animator.SetBool("Walking", false);
         m_rb.AddForce(-transform.forward * m_pushBackForce);
