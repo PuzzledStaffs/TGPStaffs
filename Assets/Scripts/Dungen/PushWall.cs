21fa4512a2e6cb50b1b7a999a32ed5a5e1a6a890
William Miller <71334596+WilliamIMiller@users.noreply.github.com>
2022-05-19 12:31:37 +0100

    push wall done

--- Assets/Scripts/Dungen/PushWall.cs
+++ Assets/Scripts/Dungen/PushWall.cs
@@ -8,29 +8,44 @@ public class PushWall : Trap
 
     [SerializeField] float m_wallDelay;
     [SerializeField] bool m_pushOnActivate;
-    Coroutine wait;
+    Coroutine m_waitCorutine;
     Animator m_animator;
+    bool m_active;
 
     private void Start()
     {
         m_animator = GetComponent<Animator>();
+        m_active = false;
     }
 
     public override void EnterRoomEnabled()
     {
+        m_active = true;
+        m_animator.enabled = true;
+
         if (m_pushOnActivate)
             m_animator.SetTrigger("Push");
+        else
+            m_waitCorutine = StartCoroutine(CWallWait());
     }
 
     public override void ExitRoomDisabled()
     {
         //End push pull sequence
-
-        //Reset position
+        StopCoroutine(m_waitCorutine);
+        m_active = false;
+        m_animator.enabled = false;
     }
 
     public void WallCycleFinished()
     {
+        if(m_active)
+            m_waitCorutine = StartCoroutine(CWallWait());
+    }
 
+    IEnumerator CWallWait()
+    {
+        yield return new WaitForSecondsRealtime(m_wallDelay + 0.5f);
+        m_animator.SetTrigger("Push");
     }
 }
