b540db2c3c138f723040c9e4d8f07d96e1a1d176
KitSymes <kitsymes@btinternet.com>
2022-05-23 15:17:24 +0100

    Dungeon Bridge and Dispenser State Saving
    - Dungeon Bridge now saves open/closed state
    - Dispenser now saves spawned object count
    - Added Int Flags to Save File

--- Assets/Scripts/Dungen/Dispencer.cs
+++ Assets/Scripts/Dungen/Dispencer.cs
@@ -8,6 +8,7 @@ public class Dispencer : Trap
 {
     [SerializeField] private GameObject m_spawnedObject;
     [SerializeField][Range(1,99)] [FormerlySerializedAs("m_MaxiumInstances")] private int m_maxiumInstances = 1;
+    private int m_spawnedObjectsCount;
     List<GameObject> m_spawnedThings = new List<GameObject>();
     [SerializeField] [FormerlySerializedAs("m_TopText")] TextMeshProUGUI m_TopText;
     [SerializeField] [Tooltip("The despencer will drop only the maximum instances, any more and it will not spawn")]
@@ -15,6 +16,10 @@ public class Dispencer : Trap
 
     private void Start()
     {
+        if (PersistentPrefs.GetInstance().m_currentSaveFile.HasIntFlag(gameObject.scene.name + "_Dispenser_" + gameObject.scene.name + "_" + gameObject.transform.parent.parent.name + "_" + gameObject.name))
+        {
+            m_spawnedObjectsCount = PersistentPrefs.GetInstance().m_currentSaveFile.GetIntFlag(gameObject.scene.name + "_Dispenser_" + gameObject.scene.name + "_" + gameObject.transform.parent.parent.name + "_" + gameObject.name);
+        }
         m_TopText.text = m_maxiumInstances.ToString();
     }
 
@@ -30,9 +35,10 @@ public class Dispencer : Trap
     public void SpawnItem()
     {
         //Spawn Item
-        if(m_spawnedThings.Count < m_maxiumInstances)
+        if(m_spawnedObjectsCount < m_maxiumInstances)
         {
             SpawnObject();
+            PersistentPrefs.GetInstance().m_currentSaveFile.SetIntFlag(gameObject.scene.name + "_Dispenser_" + gameObject.scene.name + "_" + gameObject.transform.parent.parent.name + "_" + gameObject.name, m_spawnedObjectsCount);
         }
         else if(!m_NoReset)
         {       
@@ -60,5 +66,6 @@ public class Dispencer : Trap
     private void SpawnObject()
     {
         m_spawnedThings.Add(Instantiate(m_spawnedObject, transform.forward  + transform.position, transform.rotation));
+        m_spawnedObjectsCount++;
     }
 }
