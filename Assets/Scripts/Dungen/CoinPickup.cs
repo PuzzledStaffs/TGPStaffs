7e9cb38ec597430dbc24959aa9104dcb13461ac8
Gabi4213 <71651109+Gabi4213@users.noreply.github.com>
2022-05-26 14:41:08 +0100

    finished sounds

--- Assets/Scripts/Dungen/CoinPickup.cs
+++ Assets/Scripts/Dungen/CoinPickup.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class CoinPickup : MonoBehaviour
 {
     [SerializeField] Vector2Int m_rangeOfCoinsSpawned;
+    public AudioClip m_pickupSound;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player")
         {
+            other.gameObject.GetComponent<PlayerController>().m_audioSource.PlayOneShot(m_pickupSound);
             other.gameObject.GetComponent<PlayerController>().AddCoins(Random.Range(m_rangeOfCoinsSpawned.x, m_rangeOfCoinsSpawned.y));
             Destroy(gameObject);
         }
