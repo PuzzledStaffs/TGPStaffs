d7fecfeb05a626409a8e858f18f5c82a00145d47
KitSymes <kitsymes@btinternet.com>
2022-05-27 13:50:04 +0100

    Saves to Serialised Classes, Fixes
    - Saves to Serialised Classes
    - Enabled Level Loaders
    - Assigned Player to Dungeon Camera Controller

--- Assets/Scripts/Player/PersistentPrefs.cs
+++ Assets/Scripts/Player/PersistentPrefs.cs
@@ -63,22 +63,29 @@ public class PersistentPrefs
 
     public bool HasSaveFile(int save)
     {
-        return File.Exists(Application.persistentDataPath + "/SaveFile" + save + ".json");
+        return File.Exists(Application.persistentDataPath + "/SaveFile" + save + ".save");
     }
 
     public SaveFile LoadSaveFile(int save)
     {
-        if (File.Exists(Application.persistentDataPath + "/SaveFile" + save + ".json"))
+        if (File.Exists(Application.persistentDataPath + "/SaveFile" + save + ".save"))
         {
-            return JsonUtility.FromJson<SaveFile>(File.ReadAllText(Application.persistentDataPath + "/SaveFile" + save + ".json"));
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(Application.persistentDataPath + "/SaveFile" + save + ".save", FileMode.Open);
+            SaveFile saveFile = (SaveFile)bf.Deserialize(file);
+            file.Close();
+
+            return saveFile;
         }
         return null;
     }
 
     public void SaveSaveFile(int save)
     {
-        string saveData = JsonUtility.ToJson(m_currentSaveFile);
-        File.WriteAllText(Application.persistentDataPath + "/SaveFile" + save + ".json", saveData);
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Create(Application.persistentDataPath + "/SaveFile" + save + ".save");
+        bf.Serialize(file, m_currentSaveFile);
+        file.Close();
     }
     #endregion
 
